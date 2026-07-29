668810567663d650161776e44a94742b7092963c
PROTIK-HIVE <youok1040@gmail.com>
2026-07-29 14:11:21 +0600

    Create bot.js

---
+++ scripts/cmds/bot.js
@@ -0,0 +1,9 @@
+module.exports.config = {
+  name: "bot",
+  author: "Pratik Shah",
+  description: "বট সম্পর্কিত তথ্য"
+};
+
+module.exports.onStart = async function({ api, event }) {
+  return api.sendMessage("🤖 আমি DIABLO BOT! প্রতীক শাহ বসের তৈরি সার্ভিস বট। কীভাবে সাহায্য করতে পারি?", event.threadID, event.messageID);
+};
