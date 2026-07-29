73ee36b8e1a7f54c489d4c13932ca639405375d1
PROTIK-HIVE <youok1040@gmail.com>
2026-07-29 14:36:58 +0600

    Create mathgame.js

---
+++ scripts/cmds/mathgame.js
@@ -0,0 +1,11 @@
+module.exports.config = {
+  name: "mathgame",
+  author: "Pratik Shah",
+  description: "সহজ ম্যাথ গেম"
+};
+
+module.exports.onStart = async function({ api, event }) {
+  const num1 = Math.floor(Math.random() * 50) + 1;
+  const num2 = Math.floor(Math.random() * 50) + 1;
+  return api.sendMessage(`🧮 **Math Game:** বলতো দেখি, ${num1} + ${num2} = কত?\n(ঝটপট উত্তর দে মামা!)`, event.threadID, event.messageID);
+};
