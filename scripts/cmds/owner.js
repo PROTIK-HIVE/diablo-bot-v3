72f950751e18a5176131177ea6afbbf4881c31ac
PROTIK-HIVE <youok1040@gmail.com>
2026-07-29 14:43:54 +0600

    Create owner.js

---
+++ scripts/cmds/owner.js
@@ -0,0 +1,9 @@
+module.exports.config = {
+  name: "owner",
+  author: "Pratik Shah",
+  description: "বট অনার তথ্য"
+};
+
+module.exports.onStart = async function({ api, event }) {
+  return api.sendMessage("👑 **DIABLO Owner:**\nপ্রতীক শাহ (Pratik Shah) - অফিশিয়াল লেখক এবং Diablo Bot-এর রূপকার!", event.threadID, event.messageID);
+};
