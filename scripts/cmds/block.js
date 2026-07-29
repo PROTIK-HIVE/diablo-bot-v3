445526eb91329088e4ec8cccb8763829d9cb3635
PROTIK-HIVE <youok1040@gmail.com>
2026-07-29 14:08:38 +0600

    Create block.js

---
+++ scripts/cmds/block.js
@@ -0,0 +1,10 @@
+module.exports.config = {
+  name: "block",
+  author: "Pratik Shah",
+  description: "ইউজার ব্লক করা"
+};
+
+module.exports.onStart = async function({ api, event, args }) {
+  if (!args[0]) return api.sendMessage("⚠️ কাকে ব্লক করতে চাও তা উল্লেখ করো!", event.threadID, event.messageID);
+  return api.sendMessage(`🚫 ${args.join(" ")} কে সফলভাবে ব্লক করা হয়েছে!`, event.threadID, event.messageID);
+};
