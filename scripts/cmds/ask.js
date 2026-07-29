f627e8cdaf89b62114ab20d98e5438cbb810821a
PROTIK-HIVE <youok1040@gmail.com>
2026-07-29 14:07:18 +0600

    Create ask.js

---
+++ scripts/cmds/ask.js
@@ -0,0 +1,11 @@
+module.exports.config = {
+  name: "ask",
+  author: "Pratik Shah",
+  description: "প্রশ্ন জিজ্ঞাসা করা"
+};
+
+module.exports.onStart = async function({ api, event, args }) {
+  const question = args.join(" ");
+  if (!question) return api.sendMessage("❓ প্রশ্নটি লেখো!", event.threadID, event.messageID);
+  return api.sendMessage("💭 ভেবে দেখছি... উত্তর হলো: ১০০% সত্যি!", event.threadID, event.messageID);
+};
