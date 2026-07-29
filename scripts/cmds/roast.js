be6155500f3139da584c5ecf95f85994ec8c3083
PROTIK-HIVE <youok1040@gmail.com>
2026-07-29 10:48:20 +0600

    Create roast.js

---
+++ scripts/cmds/roast.js
@@ -0,0 +1,16 @@
+module.exports.config = {
+  name: "roast",
+  author: "Pratik Shah",
+  description: "আইকনিক রোস্টিং কমান্ড"
+};
+
+const roasts = [
+  "তোর মতো আবালরে রোস্ট করতে গিয়া আমার আইকিউ লেভেল ১০ গুণ কইমা যাইবো! 🥱",
+  "তোর মাথায় যে বুদ্ধিসুদ্ধি নাই, এইটা কি তুই জন্ম থেকেই জানস নাকি ইদানীং আবিষ্কার করছস? 🗿",
+  "আমারে খোঁচা দিতে আসিস না মামা, Diablo Bot-এর রোস্টিং সহ্য করার ক্ষমতা তোর ডাটা প্যাকের নাই! 🔥"
+];
+
+module.exports.onStart = async function({ api, event }) {
+  const randomRoast = roasts[Math.floor(Math.random() * roasts.length)];
+  return api.sendMessage(randomRoast, event.threadID, event.messageID);
+};
