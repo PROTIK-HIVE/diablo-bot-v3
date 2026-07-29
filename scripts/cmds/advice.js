8687427ae0d38aef312ec9e0f4d0d86282eef30d
PROTIK-HIVE <youok1040@gmail.com>
2026-07-29 14:05:46 +0600

    Create advice.js

---
+++ scripts/cmds/advice.js
@@ -0,0 +1,9 @@
+module.exports.config = {
+  name: "advice",
+  author: "Pratik Shah",
+  description: "লাইফ এডভাইস"
+};
+
+module.exports.onStart = async function({ api, event }) {
+  return api.sendMessage("💡 **Advice:** অন্যের ড্রামায় মাথা না ঘামিয়ে চিপস আর কোক খেয়ে চিল করো! 🔥", event.threadID, event.messageID);
+};
