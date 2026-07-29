3452435be9cd02e15716e2204287d5e142079188
PROTIK-HIVE <youok1040@gmail.com>
2026-07-29 14:42:03 +0600

    Create meme.js

---
+++ scripts/cmds/meme.js
@@ -0,0 +1,14 @@
+module.exports.config = {
+  name: "meme",
+  author: "Pratik Shah",
+  description: "মিম মেসেজ"
+};
+
+module.exports.onStart = async function({ api, event }) {
+  const memes = [
+    "🐸 যখন তুই ভাবছিলি তুই গ্রুপের প্রো, ঠিক তখনই প্রতীক বস চ্যাটে ঢুকলো!",
+    "🗿 তুই যখন কাউকে বিশাল রচনা লিখে মেসেজ দিবি আর সে উত্তর দেবে 'হ'!",
+    "😂 পরীক্ষা শেষ হওয়ার পর যখন মনে হয় ভালো হইছে, কিন্তু রেজাল্টের দিন কপাল পোড়ে!"
+  ];
+  return api.sendMessage(memes[Math.floor(Math.random() * memes.length)], event.threadID, event.messageID);
+};
