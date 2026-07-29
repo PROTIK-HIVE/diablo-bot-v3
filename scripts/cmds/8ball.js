878803149e0cd9c257b1b7168d315a7386a76f8c
PROTIK-HIVE <youok1040@gmail.com>
2026-07-29 14:03:22 +0600

    Create 8ball.js

---
+++ scripts/cmds/8ball.js
@@ -0,0 +1,11 @@
+module.exports.config = {
+  name: "8ball",
+  author: "Pratik Shah",
+  description: "Magic 8-Ball"
+};
+
+module.exports.onStart = async function({ api, event, args }) {
+  const answers = ["হ্যাঁ, অবশ্যই!", "ভুলে যাও!", "সম্ভাবনা কম।", "হতেও পারে!", "নিশ্চিতভাবে বলা যাচ্ছে না।"];
+  const ans = answers[Math.floor(Math.random() * answers.length)];
+  return api.sendMessage(`🎱 **Magic 8-Ball:** ${ans}`, event.threadID, event.messageID);
+};
